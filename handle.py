c3d486568daa0d214eec5ac874217011a8294f2c
sd2880358 <leedonhom@gmail.com>
2024-04-19 14:02:03 -0600

    typo

--- handle.py
+++ handle.py
@@ -3,6 +3,7 @@
 
 import hashlib
 import web
+import os
 
 class Handle(object):
     def GET(self):
@@ -14,7 +15,7 @@ class Handle(object):
             timestamp = data.timestamp
             nonce = data.nonce
             echostr = data.echostr
-            token = "xxxx" #请按照公众平台官网\基本配置中信息填写
+            token = os.environ['wc_token'] #请按照公众平台官网\基本配置中信息填写
 
             list = [token, timestamp, nonce]
             list.sort()
