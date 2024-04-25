6dc9c3293e941c17e9ef43833385cc73c7abc8a9
sd2880358 <leedonhom@gmail.com>
2024-04-24 22:06:56 -0600

    allow traffic

--- mainProject/mainProject/settings.py
+++ mainProject/mainProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-51#g&*)l0#&!^qq_4689uf@je3yesngs&r40fdt11bkd1#ymg6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
